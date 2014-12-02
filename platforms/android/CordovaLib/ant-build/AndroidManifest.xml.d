d3662225d357c0f51b52ea53c5fb16b45f230f89
Andreas Törnström Andersson <andreas.tornstrom.andersson@gmail.com>
2014-12-02 21:25:21 +0200

    Initial commit

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/vagrant/travMsg/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/home/vagrant/travMsg/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ platforms/android/assets/www/scripts/custom.js
@@ -0,0 +1,75 @@
+'use strict';
+var ApplicationConfiguration = (function() {
+    var applicationModuleName = 'angularjsapp';
+    var applicationModuleVendorDependencies = ['ngResource', 'ngCookies', 'ngAnimate', 'ngTouch', 'ngSanitize', 'ui.router', 'ui.bootstrap', 'ui.utils'];
+    var registerModule = function(moduleName) {
+        angular
+            .module(moduleName, []);
+        angular
+            .module(applicationModuleName)
+            .requires
+            .push(moduleName);
+    };
+
+    return {
+        applicationModuleName: applicationModuleName,
+        applicationModuleVendorDependencies: applicationModuleVendorDependencies,
+        registerModule: registerModule
+    };
+})();
+
+'use strict';
+
+angular
+    .module(ApplicationConfiguration.applicationModuleName, ApplicationConfiguration.applicationModuleVendorDependencies);
+
+angular
+    .module(ApplicationConfiguration.applicationModuleName)
+    .config(['$locationProvider',
+        function($locationProvider) {
+            $locationProvider.hashPrefix('!');
+        }
+    ]);
+angular
+    .element(document)
+    .ready(function() {
+        if (window.location.hash === '#_=_') {
+            window.location.hash = '#!';
+        }
+        angular
+            .bootstrap(document,
+                [ApplicationConfiguration.applicationModuleName]);
+    });
+
+'use strict';
+
+ApplicationConfiguration.registerModule('core');
+
+'use strict';
+
+angular
+    .module('core')
+    .config(['$stateProvider',
+        '$urlRouterProvider',
+        function($stateProvider, $urlRouterProvider) {
+
+            $urlRouterProvider.otherwise('/');
+
+                        $stateProvider
+                .state('home', {
+                    url: '/',
+                    templateUrl: 'modules/core/views/home.html',
+                    controller: 'HomeController'
+                });
+        }
+    ]);
+
+'use strict';
+
+angular
+    .module('core')
+    .controller('HomeController', ['$scope',
+        function($scope) {
+
+        }
+    ]);
